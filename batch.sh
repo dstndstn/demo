2c0fc9ecd4088eb456e6dbb8bb0f35c2c6e62bb5
Dustin Lang <dstndstn@gmail.com>
2019-05-10 12:42:09 -0400

    figure out multi-node MPI

--- batch.sh
+++ batch.sh
@@ -4,13 +4,17 @@
 
 #SBATCH -p debugq
 #SBATCH -t 5:00
-#SBATCH -N 1
+#SBATCH -N 2
 
 echo "batch.sh running on $(hostname)"
 
-module load singularity
 module load mpich
 
-mpirun -n 20 singularity run myproject.sif python3 /demo/sampler.py --mpi
+# Because we need the environment set up by the "module load", we can't do this
+# as a one-liner.  Instead, we need to use another script, run-singularity.sh
+#module load singularity
+#mpirun -n 20 singularity run myproject.sif python3 /demo/sampler.py --mpi
+
+mpirun -n 80 ./run-singularity.sh
 
 echo "All done!"
